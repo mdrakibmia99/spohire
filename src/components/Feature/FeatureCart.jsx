2026d0c44460d24c0175911c39687ab12622b4f6
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-11-07 14:41:44 +0600

    cart position fixed

--- src/components/Feature/FeatureCart.jsx
+++ src/components/Feature/FeatureCart.jsx
@@ -8,32 +8,32 @@ const FeatureCart = () => {
                     <p className="lg:text-lg text-[9.50px]">I can tell you first hand, ChainGPT is absolutely amazing. They have helped me and my team speed up our working process by 125%. The access to blockchain information was never easier.</p>
                 </div>
 
-                <div className="lg:w-[378px] w-[158px] absolute lg:right-[-85px] right-[-30px]">
+                <div className="lg:w-[378px] w-[158px] absolute lg:right-[-85px] right-[-30px] bottom-0">
                     <img src="/assets/feature/feature1.png" alt="" />
                 </div>
 
             </div>
-             {/* third card  */}
+             {/* first card  */}
              <div className="lg:w-[560px] lg:h-[360px] w-[297px] h-[191px] bg-[url('/assets/feature/bg-2.png')] bg-no-repeat grid items-center relative">
                 <div className=" lg:w-[273px] w-[144px] lg:ml-[50px] ml-[26px]">
                     <h1 className="lg:text-3xl text-[17px] mb-[13px] font-bold">Stay in Touch</h1>
                     <p className="lg:text-lg text-[9.50px]">I can tell you first hand, ChainGPT is absolutely amazing. They have helped me and my team speed up our working process by 125%. The access to blockchain information was never easier.</p>
                 </div>
 
-                <div className="lg:w-[378px] w-[158px] absolute lg:right-[-85px] right-[-25px]">
-                    <img src="/assets/feature/feature2.png" alt="" />
+                <div className="lg:w-[378px] w-[158px]  lg:h-[360px] h-[191px] absolute lg:left-[235px] left-[157px] bottom-0">
+                    <img className="w-full h-full" src="/assets/feature/feature2.png" alt="" />
                 </div>
 
             </div>
-             {/* Third card  */}
+             {/* second card  */}
              <div className="lg:w-[560px] lg:h-[360px] w-[297px] h-[191px] bg-[url('/assets/feature/bg-3.png')] bg-no-repeat grid items-center relative">
                 <div className=" lg:w-[273px] w-[144px] lg:ml-[50px] ml-[26px]">
                     <h1 className="lg:text-3xl text-[17px] mb-[13px] font-bold">Confidentiality</h1>
                     <p className="lg:text-lg text-[9.50px]">I can tell you first hand, <br /> ChainGPT is absolutely amazing. They have helped me and my team speed up our working process by 125%. The access to blockchain information was never easier.</p>
                 </div>
 
-                <div className="lg:w-[378px] w-[158px] absolute lg:right-[-85px] right-[-30px]">
-                    <img src="/assets/feature/feature3.png" alt="" />
+                <div className="lg:w-[378px] w-[158px] absolute lg:right-[-85px] right-[-30px] bottom-0">
+                    <img className="w-full h-full" src="/assets/feature/feature3.png" alt="" />
                 </div>
 
             </div>
